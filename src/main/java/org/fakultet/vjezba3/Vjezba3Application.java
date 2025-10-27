4241ec5487cf3ef01e2ba6aa0f54fd6fad3dc1af
daddyDOT <ahmed.spahic.23@size.ba>
2025-10-27 12:43:21 +0100

    Initial commit

---
+++ src/main/java/org/fakultet/vjezba3/Vjezba3Application.java
@@ -0,0 +1,13 @@
+package org.fakultet.vjezba3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vjezba3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vjezba3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/fakultet/vjezba3/Vjezba3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fakultet.vjezba3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vjezba3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
